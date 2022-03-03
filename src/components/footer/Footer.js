dc2ddf63640dff636cdc93d1335843a0b337cff5
wilson-varela <53017946+wilson-varela@users.noreply.github.com>
2022-03-03 09:28:32 +0200

    Criando o footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import classes from './Footer.module.scss';
 
 const Footer = ()=>{
-    return <p>Footer</p>;
+    return (
+        <div className={`container-fluid`}>
+            <div className={`row ${classes.Footer}`}>
+                <footer>
+                    <div className="col-12">
+                        <span>&copy;Colab Pub Inc. Todos direitos reservados</span>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
 
 }
 export default Footer;
